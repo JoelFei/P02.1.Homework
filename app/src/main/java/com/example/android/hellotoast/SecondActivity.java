1715169b6c218635ae2534c31d9b4b23ac227f7a
Asus <chinjf-wm19@student.tarc.edu.my>
2020-07-30 09:48:58 +0800

    commit by joel

---
+++ app/src/main/java/com/example/android/hellotoast/SecondActivity.java
@@ -0,0 +1,39 @@
+package com.example.android.hellotoast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second) ;
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.hello);
+        textView.setText(message);
+    }
+
+}
+
+    /*
+     * Shows a Toast when the TOAST button is clicked.
+     *
+     * @param view The view that triggered this onClick handler.
+     *             Since a toast always shows on the top,
+     *             the passed in view is not used.
+     */
+
+    /*
+     * Increments the number in the TextView when the COUNT button is clicked.
+     *
+     * @param view The view that triggered this onClick handler.
+     *             Since the count always appears in the TextView,
+     *             the passed in view is not used.
+     */
